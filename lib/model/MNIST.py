e75078546e32b88f3b6723565a832125b4f15cdb
colmar <zlicheng@sjtu.edu.cn>
2022-11-23 16:55:33 +0800

    add test

--- lib/model/MNIST.py
+++ lib/model/MNIST.py
@@ -3,8 +3,16 @@ import torch.nn as nn
 
 
 class MNIST(nn.Module):
-    def __init__(self):
+    def __init__(self, num_class):
         super(MNIST, self).__init__()
+        self.layer=nn.Sequential(nn.Linear(784, 512), nn.ReLU(), nn.Linear(512, num_class))
+        self.compute_loss = nn.CrossEntropyLoss()
 
-    def forward(self, images, labels, step_idx, mode="train"):
-        return 0, 0
+    def forward(self, images, labels):  # , step_idx, mode="train"):
+        # images: [B, 1, 28, 28]
+        # labels: [B]
+        B = images.shape[0]
+        x = images.reshape(B, -1)
+        x = self.layer(x)
+        loss = self.compute_loss(x, labels)
+        return x, loss
--- train_mnist.py
+++ train_mnist.py
@@ -1,7 +1,9 @@
+import os
 import torch
 import torch.nn as nn
 import time
 import argparse
+import datetime
 import torchvision
 import torchvision.transforms as transforms
 from lib.model.MNIST import MNIST
@@ -12,7 +14,7 @@ def SVM_worker(arg):
     return 0
 
 
-def ANN_worker(arg):
+def ANN_worker(arg, save_dir):
     train_dataset = torchvision.datasets.MNIST(root='./data',
                                                train=True,
                                                transform=transforms.ToTensor(),
@@ -21,21 +23,59 @@ def ANN_worker(arg):
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                                batch_size=arg.batch_size,
                                                shuffle=True)
-    model = MNIST()
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    test_dataset = torchvision.datasets.MNIST(root='./data',
+                                              train=False,
+                                              transform=transforms.ToTensor())
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                              batch_size=arg.batch_size,
+                                              shuffle=False)
+    model = MNIST(num_class=10)
+    optimizer = torch.optim.Adam(model.parameters(), lr=arg.learning_rate)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, arg.decay_step, arg.decay_gamma)
+    print(f"Start training from epoch 0 to {arg.epoch_size}")
     for epoch_idx in range(arg.epoch_size):
         model.train()
         train_bar = etqdm(train_loader)
         for bidx, (images, labels) in enumerate(train_bar):
-            step_idx = epoch_idx * len(train_loader) + bidx
+            # step_idx = epoch_idx * len(train_loader) + bidx
 
-            pred, loss = model(images, labels, step_idx, 'train')
+            pred, loss = model(images, labels)  # , step_idx, 'train')
 
-            train_bar.set_description(f"{bar_perfixes['train']} Epoch {epoch_idx}")
+            loss_show = ('%.12f' % loss)
+            train_bar.set_description(f"{bar_perfixes['train']} Epoch {epoch_idx} Loss {loss_show}")
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+
+        scheduler.step()
+        print(f"Current LR: {[group['lr'] for group in optimizer.param_groups]}")
+
+    if arg.is_val is True:
+        print("do validation")
+
+    print("do test and compute accuracy")
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        model.eval()
+        test_bar = etqdm(test_loader)
+        for bidx, (images, labels) in enumerate(test_bar):
+            pred, _ = model(images, labels)
+            _, predicted = torch.max(pred.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            acc = 100 * correct / total
+        print('Accuracy on test set: {} %'.format(acc))
+        save_acc_path = os.path.join(save_dir, 'acc_txt')
+        with open(save_acc_path, 'w') as ff:
+            ff.write("Correct:" + str(correct) + '\n')
+            ff.write("Accuracy:" + str(acc) + '\n')
+
+    print("beginning save checkpoints")
+    save_path = os.path.join(save_dir, 'model.ckpt')
+    torch.save(model.state_dict(), save_path)
+    print("successfully save checkpoints")
 
 
 if __name__ == '__main__':
@@ -43,11 +83,37 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--type', type=str, default='ANN', choices=['SVM,ANN'])
     parser.add_argument('-b', '--batch_size', type=int, default=8)
     parser.add_argument('-e', '--epoch_size', type=int, default=100)
+    parser.add_argument('-lr', '--learning_rate', type=float, default=0.001)
+    parser.add_argument('-ds', '--decay_step', type=int, default=50)
+    parser.add_argument('-dg', '--decay_gamma', type=float, default=0.1)
+    parser.add_argument('-v', '--is_val', type=bool, default=True)
 
     arg = parser.parse_args()
+    print(f"Start training with {arg.type}")
+    datetime = datetime.datetime.now()
+    start = time.time()
     if arg.type == 'SVM':
+        save_dir = './exp/SVM'
         SVM_worker(arg)
     elif arg.type == 'ANN':
-        ANN_worker(arg)
+        save_name = f"train_ep{arg.epoch_size}_bs{arg.batch_size}_lr{arg.learning_rate}_" \
+                    f"ds{arg.decay_step}dg{arg.decay_gamma}_" \
+                    f"{datetime.year}_{datetime.month}{datetime.day}_{datetime.hour}{datetime.minute}"
+        save_dir = os.path.join('./exp/ANN', save_name)
+        os.mkdir(save_dir)
+        hype_dir = os.path.join(save_dir, 'Hyperparameters.txt')
+        with open(hype_dir,'w') as f:
+            f.write("ANN_Hyperparameters:" + '\n')
+            f.write("epoch_size:" + str(arg.epoch_size) + '\n')
+            f.write("batch_size:" + str(arg.batch_size) + '\n')
+            f.write("decay_step:" + str(arg.decay_step) + '\n')
+            f.write("decay_gamma:" + str(arg.decay_gamma) + '\n')
+            f.write("is_do_validation:" + str(arg.is_val) + '\n')
+        ANN_worker(arg, save_dir)
     else:
         raise ValueError("{} is not supported ".format(arg.type))
+    end = time.time()
+    print('Running time: %s Seconds' % (end - start))
+    save_time_dir = os.path.join(save_dir, 'RunningTime.txt')
+    with open(save_time_dir, 'w') as fff:
+        fff.write('Running time: %s Seconds' % (end - start))
