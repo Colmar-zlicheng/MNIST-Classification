ae1c0d4767d2bcc6a4913a51488b7f0837915648
colmar <zlicheng@sjtu.edu.cn>
2022-11-24 17:08:29 +0800

    compute param_size

--- lib/model/MNIST.py
+++ lib/model/MNIST.py
@@ -1,13 +1,16 @@
 import torch
 import torch.nn as nn
+from lib.utils.misc import param_size
 
 
 class MNIST(nn.Module):
     def __init__(self, num_class):
         super(MNIST, self).__init__()
+        self.name = type(self).__name__
         self.layer0 = nn.Sequential(nn.Linear(784, 512), nn.ReLU(), nn.Linear(512, 784))
         self.layer = nn.Sequential(nn.Linear(784, 512), nn.ReLU(), nn.Linear(512, num_class))
         self.compute_loss = nn.CrossEntropyLoss()
+        print(f"{self.name} has {param_size(self)}M parameters")
 
     def forward(self, images, labels):  # , step_idx, mode="train"):
         # images: [B, 1, 28, 28]
--- lib/utils/misc.py
+++ lib/utils/misc.py
@@ -5,3 +5,8 @@ bar_perfixes = {
     "val": colored("val", "yellow", attrs=["bold"]),
     "test": colored("test", "magenta", attrs=["bold"]),
 }
+
+
+def param_size(net):
+    # ! treat all parameters to be float
+    return sum(p.numel() for p in net.parameters()) * 4 / (1024 * 1024)
